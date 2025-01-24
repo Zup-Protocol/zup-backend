93c21d637f12ff8553f40e3998ce9650c35813c6
Rami Gonzalez <ramirogc21@gmail.com>
2025-01-23 22:12:09 -0300

    support multiple graphs

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,9 +1,24 @@
 import { Global, Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { GraphQLService } from './services/graphql.service';
+import { Networks } from '@/features/tokens/network.enum';
 
 @Global()
 @Module({
-  providers: [GraphQLService],
+  providers: [
+    {
+      provide: GraphQLService,
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        // Define multiple endpoints
+        const endpoints = {
+          [Networks.SEPOLIA]: configService.get<string>('GRAPHQL_URL_SEPOLIA'),
+          [Networks.SCROLL]: configService.get<string>('GRAPHQL_URL_SCROLL'),
+        };
+        return new GraphQLService(endpoints);
+      },
+    },
+  ],
   exports: [GraphQLService],
 })
 export class CoreModule {}
