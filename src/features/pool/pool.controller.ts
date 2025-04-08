8ca99e1860cdfd26f672231290c8e97d5c8527eb
Zup Protocol <zupprotocol@Ryans-MacBook-Pro.local>
2025-04-08 20:48:02 -0300

    feat: add pool filtering by tvl

--- src/features/pool/pool.controller.ts
+++ src/features/pool/pool.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
-import { PoolService } from './pool.service';
-import { Networks } from '../tokens/network.enum';
+import { Controller, Get, Param, ParseFloatPipe, Query } from '@nestjs/common';
 import { TokenMetadata } from '../tokens/dto/token.dto';
+import { Networks } from '../tokens/network.enum';
+import { PoolService } from './pool.service';
 
 @Controller('pools')
 export class PoolController {
@@ -12,11 +12,13 @@ export class PoolController {
     @Query('token0') token0: string,
     @Query('token1') token1: string,
     @Query('network') network: Networks,
+    @Query('minTvlUsd', ParseFloatPipe) minTvlUsd: number,
   ): Promise<any> {
     const bestYields = await this.poolService.findBestYieldsForTokenPair(
       token0,
       token1,
       network,
+      minTvlUsd,
     );
     return {
       bestYields,
