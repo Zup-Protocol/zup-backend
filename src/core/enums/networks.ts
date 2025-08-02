d7330ce8ce685018af6080ffdbe2a3638b2f21f8
Ryan Holanda <ryanholanda08@gmail.com>
2025-08-02 11:00:20 -0300

    feat: add support to base

--- src/core/enums/networks.ts
+++ src/core/enums/networks.ts
@@ -4,7 +4,7 @@ export enum Networks {
   ETHEREUM = 1,
   SCROLL = 534352,
   SEPOLIA = 11155111,
-  // BASE = 8453,
+  BASE = 8453,
   UNICHAIN = 130,
   // BNB = 56,
 }
@@ -24,8 +24,8 @@ export class NetworksUtils {
         return AlchemyNetwork.SCROLL_MAINNET;
       case Networks.SEPOLIA:
         return AlchemyNetwork.ETH_SEPOLIA;
-      // case Networks.BASE:
-      //   return AlchemyNetwork.BASE_MAINNET;
+      case Networks.BASE:
+        return AlchemyNetwork.BASE_MAINNET;
       case Networks.UNICHAIN:
         return AlchemyNetwork.UNICHAIN_MAINNET;
       // case Networks.BNB:
@@ -35,18 +35,10 @@ export class NetworksUtils {
 
   static isTestnet(network: Networks): boolean {
     switch (network) {
-      case Networks.ETHEREUM:
-        return false;
-      case Networks.SCROLL:
-        return false;
       case Networks.SEPOLIA:
         return true;
-      // case Networks.BASE:
-      //   return false;
-      case Networks.UNICHAIN:
+      default:
         return false;
-      // case Networks.BNB:
-      //   return false;
     }
   }
 
@@ -58,8 +50,8 @@ export class NetworksUtils {
         return `https://subgraph.satsuma-prod.com/${process.env.GRAPHQL_API_KEY}/zup-protocol-team--156415/zup-dexs-scroll/version/1.1.20/api`;
       case Networks.SEPOLIA:
         return `https://subgraph.satsuma-prod.com/${process.env.GRAPHQL_API_KEY}/zup-protocol-team--156415/zup-dexs-sepolia/version/1.1.20/api`;
-      // case Networks.BASE:
-      //   return `https://subgraph.satsuma-prod.com/${process.env.GRAPHQL_API_KEY}/zup-protocol-team--156415/zup-dexs-base/version/1.1.20/api`;
+      case Networks.BASE:
+        return `https://subgraph.satsuma-prod.com/${process.env.GRAPHQL_API_KEY}/zup-protocol-team--156415/zup-dexs-base/version/1.1.20/api`;
       case Networks.UNICHAIN:
         return `https://subgraph.satsuma-prod.com/${process.env.GRAPHQL_API_KEY}/zup-protocol-team--156415/zup-dexs-unichain/version/1.1.20/api`;
       // case Networks.BNB:
@@ -79,8 +71,8 @@ export class NetworksUtils {
         return '0x5300000000000000000000000000000000000004';
       case Networks.SEPOLIA:
         return '0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14';
-      // case Networks.BASE:
-      //   return '0x4200000000000000000000000000000000000006';
+      case Networks.BASE:
+        return '0x4200000000000000000000000000000000000006';
       case Networks.UNICHAIN:
         return '0x4200000000000000000000000000000000000006';
       // case Networks.BNB:
