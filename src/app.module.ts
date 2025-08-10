88465a808bedf4119eacab288036e258a366b8b3
Ryan Holanda <ryanholanda08@gmail.com>
2025-08-10 15:06:55 -0300

    feat: add rate limit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { join } from 'path';
 import { AppController } from './app.controller';
 import { PoolsController } from './modules/pools/pools.controller';
@@ -17,6 +19,14 @@ import { TokensModule } from './modules/tokens/tokens.module';
       rootPath: join(__dirname, '../../', 'static'),
       serveRoot: '/static/',
     }),
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 60,
+        },
+      ],
+    }),
     TokensModule,
     PoolsModule,
     ProtocolsModule,
@@ -27,6 +37,11 @@ import { TokensModule } from './modules/tokens/tokens.module';
     PoolsController,
     ProtocolsController,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
